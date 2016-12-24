1cb60cba3732e8dbcc58ddfc3177ac6685bc50b5
ManveerBasra <m13basra@gmail.com>
2016-12-23 18:22:52 -0800

    Added comments

--- gif_generate.py
+++ gif_generate.py
@@ -7,6 +7,9 @@ import sys
 
 
 class GIFGenerator:
+    """
+    Class to convert video and subtitle file into GIF with subtitles
+    """
     def __init__(self):
         self.args = None
 
@@ -15,21 +18,28 @@ class GIFGenerator:
         self.subtitle_num = None
 
     def run(self):
+        """
+        Main function that calls all other class methods
+        """
+
+        # Call class methods to initialize class variables
         self.init_arguments()
-
         self.sub_file = load(self.args.subtitles)
-
         self.init_subtitles()
 
+        # Get output filename
         gif_filename = self.get_output_file()
 
+        # Get start and end times for subtitle
         start_time = self.sub_file[self.subtitle_num].start / 1000.0
         end_time = self.sub_file[self.subtitle_num].end / 1000.0
 
+        # Create sub-clip from main video clip
         clip = (VideoFileClip(self.args.video)
                 .subclip(start_time, end_time)
-                .resize(0.30))
+                .resize(self.args.resize[0]))
 
+        # Create subtitle to be added to sub-clip
         text = (TextClip(self.subtitle,
                          fontsize=float(self.args.font_size[0]),
                          color=self.args.font_color[0],
@@ -39,13 +49,19 @@ class GIFGenerator:
                 .set_position(('center', 'bottom'))
                 .set_duration(clip.duration))
 
+        # Merge sub-clip and subtitle
         composition = CompositeVideoClip([clip, text])
+        # If fps passed by user write .gif using fps
         if self.args.fps is not None:
             composition.write_gif(gif_filename, fps=self.args.fps[0])
         else:
             composition.write_gif(gif_filename)
 
     def init_subtitles(self):
+        """
+        Check if subtitle information passed via command line is usable.
+        Initialize class variables using command line parameters
+        """
         if self.args.display_text is not None:
             self.subtitle = self.args.display_text[0]
 
@@ -64,27 +80,44 @@ class GIFGenerator:
             sys.exit(2)
 
     def get_subtitle(self):
+        """
+        Returns subtitle from subtitle file using subtitle number
+        """
         return self.sub_file[self.subtitle_num].text
 
     def get_subtitle_num(self):
+        """
+        Returns subtitle number where subtitle from command line was found
+        """
         for i in range(len(self.sub_file)):
             if self.subtitle in self.sub_file[i].text:
                 print(self.subtitle)
                 print(self.sub_file[i].text)
                 return i
+        # if subtitle not found in file
         return None
 
     def get_output_file(self):
+        """
+        Returns output filename
+        """
+
+        # If filename is passed via command line, use that
         if self.args.gif is not None:
             filename = self.args.gif[0]
+            # Append .gif if needed
             if '.gif' not in filename:
                 filename += '.gif'
+        # Else use name of video file with .gif extension
         else:
             filename = self.args.video[:-3] + 'gif'
 
         return filename
 
     def init_arguments(self):
+        """
+        Initialize command line argument parser and define acceptable arguments
+        """
         parser = argparse.ArgumentParser(description='Generate GIFs with subtitles from video and subtitle file.')
         parser.add_argument('video',
                             help='Video file to use for GIF.')
@@ -148,6 +181,7 @@ class GIFGenerator:
                             default=[False],
                             help='Capitalize every letter in subtitle (default is False).')
 
+        # Assign command line arguments to class variable
         self.args = parser.parse_args()
 
 
